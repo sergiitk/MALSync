6c83f362f349253069de62ea5f5f504d1ee5144e
sergiitk <672669+sergiitk@users.noreply.github.com>
2025-10-29 05:24:29 +0000

    Auto Urls

--- src/pages/playerUrls.js
+++ src/pages/playerUrls.js
@@ -246,6 +246,7 @@ module.exports = {
       '*://*.mixdrop.ms/e/*',
       '*://*.mixdrop.ps/e/*',
       '*://*.mixdrop.my/e/*',
+      '*://*.mdy48tn97.com/e/*',
       // auto-mixdrop-replace-dont-remove
     ],
   },
@@ -528,6 +529,7 @@ module.exports = {
       '*://jonathansociallike.com/e/*',
       '*://mariatheserepublican.com/e/*',
       '*://jilliandescribecompany.com/e/*',
+      '*://mikaylaarealike.com/e/*',
       // auto-voe-replace-dont-remove
     ],
   },
